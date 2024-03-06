7c2c9adeeede561a5768c135570c1a02c18609f1
ImFaldi <rifaldi.indrajaya@binus.ac.id>
2024-03-06 13:29:58 +0700

    Update .hintrc and input.css, remove unused code in script.js, and modify output.css

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,3 @@
-//navbar-fixed
-window.onscroll = function () {
-    const header = document.querySelector('header');
-    const fixedNav = header.offsetTop;
-
-    if (window.pageYOffset > fixedNav) {
-        header.classList.add('navbar-fixed');
-    } else {
-        header.classList.remove('navbar-fixed');
-    }
-};
 
 // hamburger
 
